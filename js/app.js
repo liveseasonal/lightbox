43b85b0941199acbd4ce0601478db11f37392128
liveseasonal <matt@liveseasonal.com>
2016-11-05 21:15:53 -0700

    Added image to lightbox overlay

--- js/app.js
+++ js/app.js
@@ -2,23 +2,38 @@
 
 //Solution create an overlay with a large image
 
-//1. Capture the click event on a link to an image
-$("img").click(function(){
-  alert("I have been clicked")
-});
-  //1.1. Show the overlay
-  //1.2 Updated overlay with the image in the link
-  //1.3 Get child's alt attibute and set caption
+//When saving the jquery object to a variable, it is convention to add a $ in front of the variable name.
+var $overlay = $('<div id="overlay"> </div>');
+var $image = $("<img>") 
+
+$overlay.append($image)
 
 //2. Add an overlay
-var overlay = jQuery('<div id="overlay"> </div>');
-overlay.appendTo(document.body)
+$("body").append($overlay)
   //2.1 Add an image
   //2.2 A caption
 
-//3. When overlay is clicked 
-$("#overlay").click(function(){
-  $("#overlay").hide();
+
+//1. Capture the click event on a link to an image
+
+$("#imageGallery a").click(function(event){
+  event.preventDefault();
+  var imageLocation = $(this).attr("href");
+  // console.log(href);
+
+
+$image.attr("src", imageLocation);
+
+$overlay.show();
+  //1.1. Show the overlay
+  //1.2 Updated overlay with the image in the link
+  //1.3 Get child's alt attibute and set caption
 });
-  //3.1 Hide the overlaycd ~
+
+
+//3. When overlay is clicked 
+// $("#overlay").click(function(){
+//   //3.1 Hide the overlay
+//   $("#overlay").hide();
+// });
   
